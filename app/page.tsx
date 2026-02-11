00f94fe9ba55dca3ad28c952cfe806b115ddf6f4
yungflashh <streaktv8@gmail.com>
2026-02-11 23:44:49 +0100

    fix updates

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,18 @@ import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
+
+
+
+
+
+
+
+
+
+
+
+    
     <main className="min-h-screen">
       <Header />
       <Hero />
