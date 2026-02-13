ad6100c1a84e6388bd275918304d0a9356139cae
yungflashh <streaktv8@gmail.com>
2026-02-13 20:59:40 +0100

    fix updates

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import WhyChooseUs from '@/components/WhyChooseUs'
+import Features from '@/components/Features'
+import Testimonials from '@/components/Testimonials'
+import WorkWithUs from '@/components/WorkWithUs'
+import Footer from '@/components/Footer'
+
+export default function Home() {
+  
+
+
+
+
+
+
+
+
+  
+  return (
+    <main className="min-h-screen">
+      <Header />
+      <Hero />
+      <WhyChooseUs />
+      <Features />
+      <WorkWithUs />
+      <Testimonials />
+      <Footer />
+    </main>
+  )
+}
